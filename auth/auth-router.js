3bce5dd191797d84282885298cbacaab0cde38bc
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-15 10:45:37 -0600

    user validation middleware done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('./auth-model');
+
 
 router.post('/register', (req, res) => {
   // implement registration
